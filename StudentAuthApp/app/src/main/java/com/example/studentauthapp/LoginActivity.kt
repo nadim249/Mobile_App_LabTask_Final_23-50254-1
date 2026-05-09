87d3b2f3e34dc074b839d233ce4bfbcecd19acc9
nadim249 <mahmoudnadim47@gmail.com>
2026-05-09 23:09:39 +0600

    Feature:login and signup added funtionality added

--- StudentAuthApp/app/src/main/java/com/example/studentauthapp/LoginActivity.kt
+++ StudentAuthApp/app/src/main/java/com/example/studentauthapp/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.example.studentauthapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +25,52 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
           //  v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        auth = FirebaseAuth.getInstance()
+
+        val email = findViewById<EditText>(R.id.etEmail)
+        val password = findViewById<EditText>(R.id.etPassword)
+        val progress = findViewById<ProgressBar>(R.id.progressBar)
+
+        findViewById<Button>(R.id.btnLogin).setOnClickListener {
+
+            progress.visibility = View.VISIBLE
+
+            auth.signInWithEmailAndPassword(
+                email.text.toString(),
+                password.text.toString()
+            ).addOnCompleteListener {
+
+                progress.visibility = View.GONE
+
+                if (it.isSuccessful) {
+
+                    val intent = Intent(this, MainActivity::class.java)
+
+                    intent.flags =
+                        Intent.FLAG_ACTIVITY_NEW_TASK or
+                                Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+                    startActivity(intent)
+
+                } else {
+
+                    Snackbar.make(
+                        findViewById(android.R.id.content),
+                        it.exception?.message.toString(),
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+
+        findViewById<TextView>(R.id.tvRegister).setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        findViewById<TextView>(R.id.tvForgot).setOnClickListener {
+            startActivity(Intent(this, ForgotPasswordActivity::class.java))
         }
     }
 }
--- StudentAuthApp/app/src/main/java/com/example/studentauthapp/RegisterActivity.kt
+++ StudentAuthApp/app/src/main/java/com/example/studentauthapp/RegisterActivity.kt
@@ -1,20 +1,99 @@
 package com.example.studentauthapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            //v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        auth = FirebaseAuth.getInstance()
+
+        val name = findViewById<EditText>(R.id.etName)
+        val email = findViewById<EditText>(R.id.etEmail)
+        val password = findViewById<EditText>(R.id.etPassword)
+        val confirm = findViewById<EditText>(R.id.etConfirmPassword)
+        val progress = findViewById<ProgressBar>(R.id.progressBar)
+
+        findViewById<Button>(R.id.btnRegister).setOnClickListener {
+
+            val emailTxt = email.text.toString().trim()
+            val passTxt = password.text.toString().trim()
+            val confirmTxt = confirm.text.toString().trim()
+
+            when {
+                name.text.isEmpty() ||
+                        emailTxt.isEmpty() ||
+                        passTxt.isEmpty() ||
+                        confirmTxt.isEmpty() -> {
+
+                    Snackbar.make(it, "All fields required", Snackbar.LENGTH_SHORT).show()
+                }
+
+                !Patterns.EMAIL_ADDRESS.matcher(emailTxt).matches() -> {
+                    Snackbar.make(it, "Invalid email", Snackbar.LENGTH_SHORT).show()
+                }
+
+                passTxt.length < 8 -> {
+                    Snackbar.make(it, "Password must be 8 characters", Snackbar.LENGTH_SHORT).show()
+                }
+
+                passTxt != confirmTxt -> {
+                    Snackbar.make(it, "Passwords do not match", Snackbar.LENGTH_SHORT).show()
+                }
+
+                else -> {
+
+                    progress.visibility = View.VISIBLE
+
+                    auth.createUserWithEmailAndPassword(emailTxt, passTxt)
+                        .addOnCompleteListener {
+
+                            progress.visibility = View.GONE
+
+                            if (it.isSuccessful) {
+
+                                startActivity(
+                                    Intent(this, MainActivity::class.java)
+                                )
+
+                                finish()
+
+                            } else {
+
+                                Snackbar.make(
+                                    findViewById(android.R.id.content),
+                                    it.exception?.message.toString(),
+                                    Snackbar.LENGTH_LONG
+                                ).show()
+                            }
+                        }
+                }
+            }
+        }
+
+        findViewById<TextView>(R.id.tvLogin).setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
         }
     }
 }
