81daf095ab41d14ea478b7c3f1471c3a6fb5310e
nadim249 <mahmoudnadim47@gmail.com>
2026-05-09 22:54:44 +0600

    design:login and signup added

--- StudentAuthApp/app/src/main/java/com/example/studentauthapp/LoginActivity.kt
+++ StudentAuthApp/app/src/main/java/com/example/studentauthapp/LoginActivity.kt
@@ -13,7 +13,7 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+          //  v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
