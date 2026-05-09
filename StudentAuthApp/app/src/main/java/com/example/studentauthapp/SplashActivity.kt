1b44171dc34abd1f7ba9bd7306b62ca82b8db9aa
nadim249 <mahmoudnadim47@gmail.com>
2026-05-09 22:34:55 +0600

    Splash design

---
+++ StudentAuthApp/app/src/main/java/com/example/studentauthapp/SplashActivity.kt
@@ -0,0 +1,38 @@
+package com.example.studentauthapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val user = FirebaseAuth.getInstance().currentUser
+
+            if (user != null) {
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+
+            finish()
+
+        }, 2000)
+    }
+}
