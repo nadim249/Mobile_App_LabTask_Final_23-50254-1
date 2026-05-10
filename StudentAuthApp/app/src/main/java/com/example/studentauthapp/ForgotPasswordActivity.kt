6595fee06a1657792edadb138cd51f9cd49e49e0
nadim249 <mahmoudnadim47@gmail.com>
2026-05-10 12:14:29 +0600

    Feature:Complete Lab Task 12

--- StudentAuthApp/app/src/main/java/com/example/studentauthapp/ForgotPasswordActivity.kt
+++ StudentAuthApp/app/src/main/java/com/example/studentauthapp/ForgotPasswordActivity.kt
@@ -1,10 +1,15 @@
 package com.example.studentauthapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class ForgotPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,34 @@ class ForgotPasswordActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
            // v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val email = findViewById<EditText>(R.id.etEmail)
+
+        findViewById<Button>(R.id.btnReset).setOnClickListener {
+
+            FirebaseAuth.getInstance()
+                .sendPasswordResetEmail(email.text.toString())
+                .addOnCompleteListener {
+
+                    if (it.isSuccessful) {
+
+                        Toast.makeText(
+                            this,
+                            "Reset email sent",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                        finish()
+
+                    } else {
+
+                        Snackbar.make(
+                            findViewById(android.R.id.content),
+                            it.exception?.message.toString(),
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                }
         }
     }
 }
--- StudentAuthApp/app/src/main/java/com/example/studentauthapp/MainActivity.kt
+++ StudentAuthApp/app/src/main/java/com/example/studentauthapp/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.studentauthapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,47 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        auth = FirebaseAuth.getInstance()
+
+        val user = auth.currentUser
+
+        findViewById<TextView>(R.id.tvEmail).text =
+            user?.email
+
+        findViewById<TextView>(R.id.tvUid).text =
+            user?.uid?.take(8)
+
+        findViewById<TextView>(R.id.tvAvatar).text =
+            user?.email?.first()?.uppercase()
+
+        findViewById<Button>(R.id.btnLogout).setOnClickListener {
+
+            auth.signOut()
+
+            val intent = Intent(this, LoginActivity::class.java)
+
+            intent.flags =
+                Intent.FLAG_ACTIVITY_NEW_TASK or
+                        Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.btnDelete).setOnClickListener {
+
+            AlertDialog.Builder(this)
+                .setTitle("Delete Account")
+                .setMessage("Are you sure?")
+                .setPositiveButton("Yes") { _, _ ->
+
+                    user?.delete()
+
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    finish()
+                }
+                .setNegativeButton("No", null)
+                .show()
         }
     }
 }
